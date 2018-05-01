2597a56cb855bea66773e706b7d1068b514dcae2
Chattox <conradlineker@gmail.com>
2018-05-01 12:06:41 +0100

    New, pristine bot. Like untrodden snow.

---
+++ main.py
@@ -0,0 +1,14 @@
+# Robomentat
+# Discord bot for data tracking and visualisation
+# Written by Chattox
+
+import discord
+import keys
+
+description = "It is by will alone I set my mind in motion"
+
+client = discord.Client()
+
+client.run(keys.key)
+
+
